935e28df89c312beb118c98ff98076db482d7fe5
VillyL <villyliaodev@gmail.com>
2021-02-02 01:17:48 +1100

    Setting up SourceTree!

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity: MonoBehaviour
+{
+    // Setting up variables
+    public int Health;
+    public int MP;
+    public int Attack;
+    public int Defense;
+    public int MagicMight;
+    public int MagicMending;
+    public int MagicResist;
+    public int Speed;
+
+    public Entity()
+    {
+        Health = 1;
+    }
+}
